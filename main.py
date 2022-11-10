336bce8731e24c547f0c7723b5784b9af22e83b9
M.M <codingblockmag@gmail.com>
2022-11-10 19:14:49 +0100

    Added a win detection

--- main.py
+++ main.py
@@ -4,16 +4,48 @@ from functools import partial
 # Declaring global variebles
 player = 0
 playing_field_machine = []
+button_list = []
 
     # Logical part
+def check_win(i, j, win_label):
+    global playing_field_machine
+    global player
+    global button_list
+    directions = (
+        ((-1, -1), (1, 1)),
+        ((1, -1), (-1, 1)),
+        ((1, 0), (-1, 0)),
+        ((0, 1), (0, -1))
+           )
+    size_v = len(playing_field_machine)
+    size_h = len(playing_field_machine[0])
+    def_dirs = (i , j)
+
+    for dir in directions:
+        count = 0
+        for dir2 in dir:
+            x , y = dir2
+            i , j = def_dirs
+
+            while playing_field_machine[i][j] == player:
+                count += 1
+                i += y
+                j += x
+                if i >= size_v or j >= size_h:
+                    break
+                if count > 5:
+                    win_label.config(text = f"Player {player} wins!!!")
+                    
+                
 # Updates playing field
-def update(i, j, button_list):
+def update(i, j, button_list, win):
     global player
     global playing_field_machine
     if playing_field_machine[i][j] == -1:
-        player = (player + 1) % 2
         playing_field_machine[i][j] = player
         player_move(button_list[i][j])
+        check_win(i, j, win)
+        player = (player + 1) % 2
 
 
 def player_move(button):
@@ -25,7 +57,7 @@ def player_move(button):
     button.config(text=text, command=None)
 
 # Generates a playing field
-def _generate(x, y, button_list, playing_field):
+def _generate(x, y, button_list, playing_field, win):
     global playing_field_machine
     for i in range(x):
         playing_field_machine.append([])
@@ -33,7 +65,7 @@ def _generate(x, y, button_list, playing_field):
         for j in range(y):
             playing_field_machine[i].append(-1)
 
-            button_list[i].append(Button(playing_field, command=partial(update, i, j, button_list), width=1))
+            button_list[i].append(Button(playing_field, command=partial(update, i, j, button_list, win), width=1))
             button_list[i][j].grid(column=i, row=j)
 
 # Generates varieble length playing field
@@ -42,13 +74,15 @@ def generate(entry1, entry2, info_frame, error):
     try:
         h_size = int(entry1.get())
         v_size = int(entry2.get())
+        if h_size < 0 or v_size < 0:
+            isNotError = False
     except ValueError:
-        error.config(text="Please enter a positive number")
         isNotError = False
     if isNotError:
-        _generate(h_size, v_size, button_list, playing_field)
+        _generate(h_size, v_size, button_list, playing_field, error)
         info_frame.destroy()
-        error.destroy()
+    else:
+        error.config(text="Please enter a positive number")
 
     # Graphical part 
 root = Tk()
@@ -58,7 +92,7 @@ error_label = Label(root)
 info_frame = Frame(root)
 playing_field = Frame(root)
 
-button_list = []
+
 # Makes config screen
 wightEntry = Entry(info_frame)
 wightEntry.insert(0, "Insert wight")
